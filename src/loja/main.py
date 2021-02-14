2ef30fd77c77f8e979219c6d2d86b46a1378de66
Fernando <fwfurtado@gmail.com>
2021-02-14 18:49:22 -0300

    Remove unused code

--- src/loja/main.py
+++ src/loja/main.py
@@ -1,30 +1,4 @@
-from src.loja.controllers.customer_controller import CustomerController
-from src.loja.repositories.customer_dao import CustomerDAO
-
-
-def test():
-    name = "Fernando"
-    cpf = "13241234"
-
-    dao = CustomerDAO()
-    controller = CustomerController(dao)
-
-    assert len(controller.list()) == 0
-
-    controller.new_customer(name, cpf)
-
-    assert len(controller.list()) == 1
-
-    customer = controller.show(1)
-
-    assert customer.name == name
-    assert customer.social_number == cpf
-
-    print(f"Customer {customer.name} - {customer.social_number}")
-
-
-
 
 
 if __name__ == '__main__':
-    test()
+    raise NotImplemented("Has not main")
