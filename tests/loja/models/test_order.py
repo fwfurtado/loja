123627bcdc108eee3e9524df9a31f4ec9e983433
Fernando <fwfurtado@gmail.com>
2021-02-14 18:44:07 -0300

    Improve the test stack

--- tests/loja/models/test_order.py
+++ tests/loja/models/test_order.py
@@ -3,12 +3,15 @@ from loja.models.order import Order
 from loja.models.order_item import OrderItem
 from loja.models.product import Product
 
+from tests.loja.factories.product_factory import ProductFactory
+
 
 class TestOrder:
 
     def test_should_compute_total_from_items(self):
-        t_shirt = Product("TShirt", 15)
-        shorts = Product("Shorts", 25)
+
+        t_shirt = ProductFactory.create(value=15)
+        shorts = ProductFactory.create(value=25)
 
         customer = Customer("Fernando", "12341234")
         order = Order(customer)
