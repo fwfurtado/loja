89bb5b1544ed79c8892d45d08f775bff195c6d08
eiji <eijitkzw@gmail.com>
2021-02-20 13:07:04 -0300

    Foi criado o DAO para produto e seus testes

--- tests/loja/repositories/test_customer_dao.py
+++ tests/loja/repositories/test_customer_dao.py
@@ -1,20 +1,21 @@
-from src.loja.models.customer import Customer
+
 from src.loja.repositories.customer_dao import CustomerDAO
+from tests.loja.factories.customer_factory import CustomerFactory
 
 
 class TestCustomerDAO:
 
-    def test_should_increment_length(self):
-        dao = CustomerDAO()
-        customer = Customer("Fernando", "12342134")
+    def test_should_increment_length(self): # Método = Função dentro de uma classe
+        dao = CustomerDAO() # Cria instância do DAO
+        customer = CustomerFactory.create()
 
-        assert len(dao.find_all()) == 0
+        assert len(dao.find_all()) == 0 # Garantir que o len de (x) seja ==0
 
-        dao.persist(customer)
+        dao.persist(customer) # Adiciona um cliente no banco de dados
 
-        assert len(dao.find_all()) == 1
+        assert len(dao.find_all()) == 1 # Garante que o len de (x) seja 1, pois já temos um cliente no banco
 
-        read_customer = dao.find_one(1)
+        read_customer = dao.find_one(1) # Procura pelo cliente que tenha o ID = a 1
 
         assert customer.name == read_customer.name
         assert customer.social_number == read_customer.social_number
