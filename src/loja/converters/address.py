37a721fa78cea7d43de43b79259df0b6b3bd620e
Eiji <eijitkzw@gmail.com>
2021-03-07 16:14:47 -0300

    Criando converter

---
+++ src/loja/converters/address.py
@@ -0,0 +1,7 @@
+from src.loja.dtos.address_dto import AddressDTO
+from src.loja.models.address import Address
+
+
+class AddressConverter:
+    def convert(self, form: AddressDTO) -> Address:
+        return Address(form.street, form.number, form.zip_code, form.complement)
