4d642d57c8e0a7b817156439fa6779c23bb8621e
Eiji <eijitkzw@gmail.com>
2021-05-22 09:18:09 -0300

    fix typing again

--- src/loja/models/product.py
+++ src/loja/models/product.py
@@ -7,12 +7,12 @@ from decimal import Decimal
 
 class Product(Base):
     __tablename__ = "products"
-    id: Column[int] = Column(BigInteger, primary_key=True)
-    name: Column[str] = Column(String, nullable=False)
-    price: Column[Decimal] = Column(Numeric, nullable=False)
-    photo: Column[Optional[str]] = Column(String)
-    description: Column[str] = Column(String, nullable=False)
-    created_at: Column[datetime] = Column(DateTime, default=datetime.now, nullable=False)
+    id = Column(BigInteger, primary_key=True)
+    name = Column(String, nullable=False)
+    price = Column(Numeric, nullable=False)
+    photo = Column(String)
+    description = Column(String, nullable=False)
+    created_at = Column(DateTime, default=datetime.now, nullable=False)
 
     def __str__(self):
         return f"Product({self.name, self.price, self.photo, self.description})"
