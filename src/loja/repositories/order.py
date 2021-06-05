80aa69e35cda52b02cfe738702c8c6c17041b095
Patricia Ozono <paty.ozono@gmail>
2021-06-05 09:10:57 -0300

    fix order repository

--- src/loja/repositories/order.py
+++ src/loja/repositories/order.py
@@ -1,6 +1,8 @@
 from typing import List, Optional
 from src.loja.models.order import Order
 from sqlalchemy.orm import Session
+from src.loja.models.order_item import OrderItem
+from sqlalchemy.orm.query import Query
 
 
 class OrderDAO:
@@ -14,10 +16,16 @@ class OrderDAO:
             self.__session.merge(order)
 
     def find_all(self) -> List[Order]:
-        return self.__session.query(Order).all()
+        return self.__find_with_join().all()
 
     def find_one(self, id: int) -> Optional[Order]:
-        return self.__session.query(Order).filter(Order.id == id).first()
+        return self.__find_with_join().filter(Order.id == id).first()
 
     def remove(self, id: int):
         self.__session.query(Order).filter(Order.id == id).delete()
+
+    def __find_with_join(self) -> Query[Order]:
+        return (
+            self.__session.query(Order)
+            .join(OrderItem, Order.id == OrderItem.order_id)
+        )
